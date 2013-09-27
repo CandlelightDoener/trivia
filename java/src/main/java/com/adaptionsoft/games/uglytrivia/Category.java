2e15ad36270fd1c8bf5da69299b13e7c74fb3567
Seb <sebastian.heglmeier@improuv.com>
2013-09-27 10:07:37 +0200

    The Player class knowing about the category is odd. Moving it to Questions did not work out, since Players needs to know about it, too. Made an (yet unused) enum "Category".

---
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Category.java
@@ -0,0 +1,23 @@
+package com.adaptionsoft.games.uglytrivia;
+
+public enum Category {
+    POP("Pop"),
+    SCIENCE("Science"),
+    SPORTS("Sports"),
+    ROCK("Rock");
+
+    private final String categoryName;
+
+    private Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public static String getCategoryName(int field) {
+        int noOfCategories = Category.values().length;
+        return Category.values()[field % noOfCategories].getCategoryName();
+    }
+}
